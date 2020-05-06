2f23528be5facd7e49ad4cfa5d1906ba19f8ce6f
Oriana Olivetti <oriana.olivetti@phinxlab.com>
2020-05-06 12:06:01 -0300

    Documentation and some tests were added. Refactor of functions.

--- enum_operations/mix.exs
+++ enum_operations/mix.exs
@@ -7,7 +7,17 @@ defmodule EnumOperations.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "EnumOperations Tutorial",
+      source_url: "https://github.com/OlivettiOriana/Elixir-Tutorial",
+      homepage_url: "http://YOUR_PROJECT_HOMEPAGE",
+      docs: [
+        # The main page in the docs
+        main: "EnumOperations",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -22,7 +32,9 @@ defmodule EnumOperations.MixProject do
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:earmark, "~> 1.2", only: :dev},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
 end
