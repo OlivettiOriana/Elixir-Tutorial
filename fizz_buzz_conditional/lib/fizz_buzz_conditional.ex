a6a30857a1ccc5bbbc695878f14960c7ac616474
Oriana Olivetti <oriana.olivetti@phinxlab.com>
2020-05-07 09:32:43 -0300

    FizzBuzz name functions were modified.

--- fizz_buzz_conditional/lib/fizz_buzz_conditional.ex
+++ fizz_buzz_conditional/lib/fizz_buzz_conditional.ex
@@ -13,21 +13,21 @@ defmodule FizzBuzzConditional do
 
   ## Examples
 
-      iex> FizzBuzzConditional.fuzzle(0, 0, 4)
+      iex> FizzBuzzConditional.fuzzle_case(0, 0, 4)
       "FizzBuzz"
 
-      iex> FizzBuzzConditional.fuzzle(0, 3, 4)
+      iex> FizzBuzzConditional.fuzzle_case(0, 3, 4)
       "Fizz"
 
-      iex> FizzBuzzConditional.fuzzle(2, 0, 4)
+      iex> FizzBuzzConditional.fuzzle_cond(2, 0, 4)
       "Buzz"
 
-      iex> FizzBuzzConditional.fuzzle(3, 7, 4)
+      iex> FizzBuzzConditional.fuzzle_cond(3, 7, 4)
       4
 
   """
 
-  def fuzzle(arg1, arg2, arg3) do
+  def fuzzle_case(arg1, arg2, arg3) do
     case {arg1, arg2, arg3} do
       {0, 0, _} -> "FizzBuzz"
       {0, _, _} -> "Fizz"
@@ -36,12 +36,12 @@ defmodule FizzBuzzConditional do
     end
   end
 
-  # def fuzzle(arg1, arg2, arg3) do
-  #   cond do
-  #     arg1 == 0 and arg2 == 0 -> "FizzBuzz"
-  #     arg1 == 0 -> "Fizz"
-  #     arg2 == 0 -> "Buzz"
-  #     true -> arg3
-  #   end
-  # end
+  def fuzzle_cond(arg1, arg2, arg3) do
+    cond do
+      arg1 == 0 and arg2 == 0 -> "FizzBuzz"
+      arg1 == 0 -> "Fizz"
+      arg2 == 0 -> "Buzz"
+      true -> arg3
+    end
+  end
 end
--- fizz_buzz_conditional/test/fizz_buzz_conditional_test.exs
+++ fizz_buzz_conditional/test/fizz_buzz_conditional_test.exs
@@ -3,18 +3,22 @@ defmodule FizzBuzzTest do
   doctest FizzBuzzConditional
 
   test "testing FizzBuzz" do
-    assert FizzBuzzConditional.fuzzle(0, 0, 4) == "FizzBuzz"
+    assert FizzBuzzConditional.fuzzle_case(0, 0, 4) == "FizzBuzz"
+    assert FizzBuzzConditional.fuzzle_cond(0, 0, 4) == "FizzBuzz"
   end
 
   test "testing Fizz" do
-    assert FizzBuzzConditional.fuzzle(0, 3, 5) == "Fizz"
+    assert FizzBuzzConditional.fuzzle_case(0, 3, 5) == "Fizz"
+    assert FizzBuzzConditional.fuzzle_cond(0, 3, 5) == "Fizz"
   end
 
   test "testing Buzz" do
-    assert FizzBuzzConditional.fuzzle(9, 0, 8) == "Buzz"
+    assert FizzBuzzConditional.fuzzle_case(9, 0, 8) == "Buzz"
+    assert FizzBuzzConditional.fuzzle_cond(9, 0, 8) == "Buzz"
   end
 
   test "testing 3rd argument" do
-    assert FizzBuzzConditional.fuzzle(9, 4, 8) == 8
+    assert FizzBuzzConditional.fuzzle_case(9, 4, 8) == 8
+    assert FizzBuzzConditional.fuzzle_cond(9, 4, 8) == 8
   end
 end
